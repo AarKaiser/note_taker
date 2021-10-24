c5449ddc4a6b18aff88645f4aa1a51a45d56c064
Aar Kaiser <studies@mohussein.com>
2021-10-24 03:02:31 -0400

    linking notes.js to file save helper

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,10 @@
 // Required
 const notes = require('express').Router();
+const { deleteNote, readFromFile, writeToFile, readAndAppend } = require('../helpers/fsutil');
+
+// 
+
+// Route for all notes
 
 
 
