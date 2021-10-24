4918170b9f535f407bb34e4372c23bac528026b7
Aar Kaiser <studies@mohussein.com>
2021-10-24 02:33:32 -0400

    updating index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
+// Required
 const express = require('express');
 const notesRouter = require('./notes');
+
+// Declaring App
 const app = express();
 
+// Middleware
 app.use('/notes', notesRouter);
 
+// Exporting app
 module.exports = app;
