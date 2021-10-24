237679caf2ebccf73d144120915cfdaea43d0322
Aar Kaiser <studies@mohussein.com>
2021-10-24 02:28:24 -0400

    trying Heroku again

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// Wildcard
+// Route for wildcard/error/404 page
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/404.html' ))
