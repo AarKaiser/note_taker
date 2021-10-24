fa9e8a896dd78ddedcc6447bbe7a49331ea3dfab
Aar Kaiser <studies@mohussein.com>
2021-10-24 01:14:09 -0400

    heroku init

--- server.js
+++ server.js
@@ -37,10 +37,9 @@ app.listen(PORT, ()=>
 console.log(`Listening on http://localhost:${PORT} 🚀`)
 );
 
-
-// GIVEN a note-taking application
-// WHEN I open the Note Taker
-// THEN I am presented with a landing page with a link to a notes page
+// GIVEN a note-taking application 
+// WHEN I open the Note Taker 
+// THEN I am presented with a landing page with a link to a notes page ✔️
 // WHEN I click on the link to the notes page
 // THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
 // WHEN I enter a new note title and the note’s text
