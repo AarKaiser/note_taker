65798c640531909667196faef8cb39e5d6a85665
Aar Kaiser <studies@mohussein.com>
2021-10-24 23:05:54 -0400

    updating notes route

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,11 @@ const notes = require('express').Router();
 const { deleteNote, readFromFile, writeToFile, readAndAppend } = require('../helpers/fsutil');
 
 
-// Route for all notes
+// Route for retrieving all notes
+
+notes.get("/", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+});
 
 
 
