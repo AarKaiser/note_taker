009130901b3f6a78141f8359e4460744c7ae2b92
Aar Kaiser <dev@aarkaiser>
2021-10-27 00:01:03 -0400

    removing checklist from server.js file

--- server.js
+++ server.js
@@ -34,17 +34,3 @@ app.get("*", (req, res) => {
 
 // Listening on Port
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT} 🚀`));
-
-// GIVEN a note-taking application ✔️
-// WHEN I open the Note Taker ✔️
-// THEN I am presented with a landing page with a link to a notes page ✔️
-// WHEN I click on the link to the notes page ✔️
-// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column ✔️
-// WHEN I enter a new note title and the note’s text ✔️
-// THEN a Save icon appears in the navigation at the top of the page ✔️
-// WHEN I click on the Save icon ✔️
-// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes ✔️
-// WHEN I click on an existing note in the list in the left-hand column
-// THEN that note appears in the right-hand column
-// WHEN I click on the Write icon in the navigation at the top of the page
-// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
